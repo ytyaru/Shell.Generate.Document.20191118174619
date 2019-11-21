049cfc4e11589541bd91b5a140ed3503a20098db
ytyaru <ytyaru@outlook.jp>
2019-11-22 08:55:38 +0900

    パス取得の修正

---
+++ src/00_denv/01_md/01_path/getIconPath.js
@@ -0,0 +1,2 @@
+function getIconDir() { return "./src/00_denv/01_md/00_icon"; }
+function getIconPath(id) { return getIconDir() + "/" + id.toLowerCase(); } 
--- src/00_denv/01_md/03_get_font_size/get_md_img.sh
+++ src/00_denv/01_md/03_get_font_size/get_md_img.sh
@@ -3,10 +3,12 @@ GetIconMd() {
 	local PATH_DIR="$(cd $(dirname $SCRIPT_DIR); pwd)/01_path"
 	local PATH_FILE="$PATH_DIR/get_icon_path.sh"
 	. "$PATH_FILE"
-	ImgMd() { echo '!['"$1"']('"$(GetIconRealPath "$1")"" \"$1\""')'; }
+#	echo '![]('"$(GetIconPath $1)"')'
+#	ImgMd() { echo '![]('"$(GetIconPath $1)"')'; }
+#	ImgMd() { echo '![]('"$(GetIconPath "$1")"" \"$1\""')'; }
+	ImgMd() { echo '!['"$1"']('"$(GetIconPath "$1")"" \"$1\""')'; }
 	echo "$(ImgMd "$1")"
 }
 echo $(GetIconMd Linux)
 echo $(GetIconMd Debian)
 echo $(GetIconMd Raspbian)
-

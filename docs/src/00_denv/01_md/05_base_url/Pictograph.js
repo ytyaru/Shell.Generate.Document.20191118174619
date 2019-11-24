f2a9a22189a9fd2672f3c0a91924f2a7580741d0
ytyaru <ytyaru@outlook.jp>
2019-11-24 09:59:52 +0900

    baseURL

--- docs/src/00_denv/01_md/05_base_url/Pictograph.js
+++ docs/src/00_denv/01_md/05_base_url/Pictograph.js
@@ -46,5 +46,6 @@ export class Pictograph {
 //    __getIconDir() { return this.__getProjectName() + "/src/00_denv/01_md/00_icon"; } // ドメインからの相対パス短縮記法
 //    __getIconPath() { return this.__getIconDir() + "/" + this.Id.toLowerCase() + ".svg"; } 
 //    __getIconPath() { return "/src/00_denv/01_md/00_icon/" + this.Id.toLowerCase() + ".svg"; } 
-    __getIconPath() { return document.baseURI + "/src/00_denv/01_md/00_icon/" + this.Id.toLowerCase() + ".svg"; } 
+//    __getIconPath() { return document.baseURI + "/src/00_denv/01_md/00_icon/" + this.Id.toLowerCase() + ".svg"; } 
+    __getIconPath() { console.log('document.baseURI:', document.baseURI); return document.baseURI + "src/00_denv/01_md/00_icon/" + this.Id.toLowerCase() + ".svg"; } 
 }
